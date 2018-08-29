de3ebd40815c4bd833b6f70fa6cee1b32c958d2a
Florian Echtler <floe@butterbrot.org>
2018-08-29 15:02:44 +0200

    Update Application.mk

--- Assets/Plugins/Android/src/Application.mk
+++ Assets/Plugins/Android/src/Application.mk
@@ -1,4 +1,5 @@
 APP_OPTIM        := release
 APP_ABI          := armeabi
-APP_PLATFORM     := android-14
+APP_PLATFORM     := android-24
 APP_BUILD_SCRIPT := Android.mk
+APP_STL          := c++_shared
